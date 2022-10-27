780c12e43e811e70524f37c3b54c6b314629a4f4
kamiwey <kamiwey@gmail.com>
2022-10-27 14:03:53 +0000

    little changes

--- src/js/component/footer.jsx
+++ src/js/component/footer.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Foot = () => {
     return (
-        <div className="card bg-dark">
-            <div className="card-footer bg-dark"></div>
+        <div className="nav bg-dark">
+            
             <div className="card-body bg-dark">
                 <h5 className="card-title text-light text-center bg-dark">Copyrigth © Your Website 2022</h5>
             </div>
